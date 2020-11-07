451164be1f876991121abf971598c4ff49c00bbe
Egor Zorin <zorinj@gmail.com>
2020-11-07 23:56:53 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/">Kompetenzen</router-link>
+      <router-link to="/">Team</router-link>
+      <router-link to="/">Events</router-link>
+      <router-link to="/">Karriere</router-link>
+      <router-link to="/">Blog</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import "@/styles/common.scss";
+export default {
+
+}
+</script>
