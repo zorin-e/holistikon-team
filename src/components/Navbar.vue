eb7eaa5c21525dc302b9b58035f28bd250c94cc2
Egor Zorin <zorinj@gmail.com>
2020-11-09 00:58:51 +0300

    Add: navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,66 @@
+<template>
+  <nav :class="$style.navbar">
+    <logo class="mb-10" />
+    <div :class="$style.wrapper">
+      <base-menu
+        :items="menu"
+      />
+      <base-select
+        class="ml-30"
+        v-model="currentLanguage"
+        :items="languages"
+        is-circle
+      />
+    </div>
+  </nav>
+</template>
+
+<script>
+import Logo from "@/assets/logo.svg"
+import BaseSelect from "@/components/BaseSelect"
+import BaseMenu from "@/components/BaseMenu"
+
+export default {
+  name: "Navbar",
+  components: {
+    Logo,
+    BaseSelect,
+    BaseMenu
+  },
+  data() {
+    return {
+      menu: [
+        { title: 'Home', active: false },
+        { title: 'Kompetenzen', active: false },
+        { title: 'Team', active: true },
+        { title: 'Events', active: false},
+        { title: 'Karriere', active: false },
+        { title: 'Blog', active: false}
+      ],
+      languages: [
+        {
+          title: "en",
+          value: "en"
+        },
+        {
+          title: "ru",
+          value: "ru"
+        }
+      ],
+      currentLanguage: 'en'
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+.navbar {
+  height: 60px;
+  @include flex(space-between, flex-end);
+}
+
+.wrapper {
+  @include flex(null, flex-end);
+  height: 100%;
+}
+</style>
