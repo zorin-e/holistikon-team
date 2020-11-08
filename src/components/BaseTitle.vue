ec7dad602d6adea597ef46c6f0396d5d81831b5f
Egor Zorin <zorinj@gmail.com>
2020-11-09 00:57:49 +0300

    Add: title component

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2
+    :class="[$style.h2, center && $style.center]"
+  >
+    <slot />
+  </h2>
+</template>
+
+<script>
+export default {
+  name: "BaseTitle",
+  props: {
+    center: Boolean
+  }
+}
+</script>
+
+<style lang="scss" module>
+.h2 {
+  text-transform: uppercase;
+  font-weight: bold;
+  @include title;
+}
+
+.center {
+  text-align: center;
+}
+</style>
