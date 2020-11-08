a545284f284fd09289b6c6632b0d03aa086e6c78
Egor Zorin <zorinj@gmail.com>
2020-11-09 00:59:10 +0300

    Add: paragraph component

---
+++ src/components/Paragraph.vue
@@ -0,0 +1,24 @@
+<template>
+  <p :class="[$style.p, center && $style.center]"><slot /></p>
+</template>
+
+<script>
+export default {
+  name: "Paragraph",
+  props: {
+    center: Boolean
+  }
+}
+</script>
+
+<style lang="scss" module>
+.p {
+  font-size: $fieldFontSizeSm;
+  margin: 0 10px;
+  line-height: 1.8;
+}
+
+.center {
+  text-align: center;
+}
+</style>
