86405f37b67f03e09a5c40fba19673e665c46141
Egor Zorin <zorinj@gmail.com>
2020-11-08 22:29:46 +0300

    Add: base settings

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,36 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import '@/styles/_mixins.scss';
+          @import '@/styles/_variables.scss';
+        `
+      }
+    }
+  },
+  productionSourceMap: false,
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+  },
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        exclude: /\.html$/
+      })
+    ],
+    resolve: {
+      alias: {
+        '@unit': path.resolve(__dirname, 'tests/unit')
+      }
+    }
+  }
 }
