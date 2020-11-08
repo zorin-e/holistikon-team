bd654dca67e2040df409d91b7b12f1e0093fb31d
Egor Zorin <zorinj@gmail.com>
2020-11-09 01:07:22 +0300

    Add: title-line component

---
+++ src/components/TitleLine.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="$style.line"></div>
+</template>
+
+<script>
+export default {
+  name: "TitleLine"
+}
+</script>
+
+<style lang="scss" module>
+.line {
+  background: $pink;
+  @include size(45px, 8px);
+}
+</style>
+
