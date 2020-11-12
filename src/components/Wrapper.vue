c3a15ffcf8560dc54d1d5cc927daa341d5b74801
Egor Zorin <zorinj@gmail.com>
2020-11-12 17:30:17 +0300

    Add: wrapper component

---
+++ src/components/Wrapper.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="center && $style.center">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Wrapper",
+  props: {
+    center: Boolean
+  }
+}
+</script>
+
+<style module>
+.center {
+  text-align: center;
+}
+</style>
